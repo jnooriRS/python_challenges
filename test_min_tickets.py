a1ecf8cd12b2beab956db3a0a65a39b1f2d693a2
jnooriRS <jonas.noori@riversafe.co.uk>
2022-10-12 12:13:47 +0100

    both tests working

--- test_min_tickets.py
+++ test_min_tickets.py
@@ -1,16 +1,21 @@
 import unittest
-from min_tickets import mincostTickets
+from min_tickets import solution
 
 
 class TestCoinResult(unittest.TestCase):
     def test_change(self):
-        self.assertEqual(mincostTickets(5, [1, 2, 5]), 4)
+        self.assertEqual(
+            solution.mincostTickets(self, [1, 4, 5, 6, 7, 8, 20], [2, 7, 15]),
+            10
+            # purposfully created error to see what the coreect return value should be in terminal
+        )
 
     def test_input_type(self):
-        self.assertRaises(ValueError, mincostTickets("x", ["x, y, z"]), "some-string")
+        with self.assertRaises(TypeError):
+            solution.mincostTickets(self, ["x"], ["x, y, z"])
 
 
-# python -m unittest test_filename.py
+# python -m unittest test_min_tickets.py
 if __name__ == "__main__":
     unittest.main()
 
@@ -19,3 +24,11 @@ if __name__ == "__main__":
 # day 1 you buy cost[0] = 2
 # day 3 you buy cost[1] = 7
 # day 20 you buy cost[0] = 2
+
+
+def test_split(self):
+    s = "hello world"
+    self.assertEqual(s.split(), ["hello", "world"])
+    # check that s.split fails when the separator is not a string
+    with self.assertRaises(TypeError):
+        s.split(2)
